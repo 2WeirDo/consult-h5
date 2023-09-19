ccb742e7eb1eedb1efcb703dc0396c7de3d58b50
zweirdo <1606403438@qq.com>
2023-09-19 21:29:24 +0800

    cp-nav-bar组件封装--声明组件类型

---
+++ src/types/components.d.ts
@@ -0,0 +1,13 @@
+// 为项目全局组件声明类型，让写属性和事件可以有提示
+
+// 1. 导入组件实例
+import CpNavBar from '@/components/cp-nav-bar.vue'
+// 2. 声明 vue 类型模块
+declare module 'vue' {
+  // 3. 给 vue  添加全局组件类型，interface 和之前的合并
+  interface GlobalComponents {
+    // 4. 定义具体组件类型，typeof 获取到组件实例类型
+    // typeof 作用是得到对应的TS类型
+    CpNavBar: typeof CpNavBar
+  }
+}
