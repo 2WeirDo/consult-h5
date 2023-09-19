ed2fc29a6b70e20f4f61913aaea5ab77bf9217ee
zweirdo <1606403438@qq.com>
2023-09-19 19:13:10 +0800

    数据持久化自定义存储

--- src/stores/modules/user.ts
+++ src/stores/modules/user.ts
@@ -28,7 +28,15 @@ export const useUserStore = defineStore(
       delUser
     }
   },
+  // 方式一: 默认存储所有数据, 以store的ID作为存储的key
+  // {
+  //   persist: true // 开启数据持久化
+  // }
+  // 方式二: 自定义存储的key, 指定存储哪些数据
   {
-    persist: true // 开启数据持久化
+    persist: {
+      key: 'cp-user', // 这里我们不变
+      paths: ['user'] // 数组内放入的变量名都会持久化存储, 这里我们就只存了user
+    }
   }
 )
