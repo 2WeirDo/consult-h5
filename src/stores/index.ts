4ca9e6d2cccc72ffd532a48283b0ec235304738b
zweirdo <1606403438@qq.com>
2023-09-19 17:55:00 +0800

    vant按需引入+关闭自动生成类型文件

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -8,3 +8,7 @@ const pinia = createPinia()
 pinia.use(persistedstate)
 
 export default pinia
+
+// 对modules中store仓库做统一导出
+// (以后再导入store的时候就省几个字符串hhh)
+export * from './modules/user'
--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,19 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// vant按需引入
+import Components from 'unplugin-vue-components/vite'
+import { VantResolver } from '@vant/auto-import-resolver'
+
 export default defineConfig({
   // base: '/服务器中放置打包代码的目录名'
   plugins: [
     vue({
       reactivityTransform: true // 允许对父传子props解构时保留响应式
+    }),
+    Components({
+      resolvers: [VantResolver()], // vant按需引入
+      dts: false // 关闭自动生成类型声明文件(不需要了, 已经有了)
     })
   ],
   resolve: {
