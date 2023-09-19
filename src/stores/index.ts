958c3b2967bd8ce2d954e99a7c4a92d2f4e9be28
zweirdo <1606403438@qq.com>
2023-09-19 15:28:02 +0800

    开启pinia数据持久化

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,5 +1,10 @@
 import { createPinia } from 'pinia'
+// 导入持久化插件
+import persistedstate from 'pinia-plugin-persistedstate'
 
 const pinia = createPinia()
+
+// 注册持久化插件
+pinia.use(persistedstate)
 
 export default pinia
