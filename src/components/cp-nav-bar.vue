5c8df129b6b256108b470a5c8728cb7f31e802d6
zweirdo <1606403438@qq.com>
2023-09-19 21:05:22 +0800

    cp-nav-bar组件封装--功能实现

--- src/components/cp-nav-bar.vue
+++ src/components/cp-nav-bar.vue
@@ -1,26 +1,50 @@
 <script setup lang="ts">
 // 封装需求❓：支持 title rightText 属性，支持 click-right 事件，click-left函数内支持返回上一页或默认首页
+import { useRouter } from 'vue-router'
+// 1. 已有的功能：返回图标，返回效果，固定定位（组件内部实现）
+const router = useRouter()
 const onClickLeft = () => {
-  // 点击左侧执行
+  // 判断历史记录中是否有回退
+  // 补充: 当我们使用router.replace跳转是不能返回页面的
+  if (history.state?.back) {
+    // 判断是否可以回退
+    // 存在回跳地址就返回
+    router.back()
+  } else {
+    // 否则跳回首页
+    router.push('/')
+  }
 }
+// 接收父传子变量
+defineProps<{
+  title?: string
+  rightText?: string
+}>()
+
+// 子传父
+
+const emit = defineEmits<{
+  (e: 'click-right'): void
+}>()
 const onClickRight = () => {
-  //
+  // 传递自定义事件click-right给父组件
+  emit('click-right')
 }
 </script>
 
 <template>
-  <!-- 二次封装 -->
   <van-nav-bar
     left-arrow
     @click-left="onClickLeft"
     fixed
-    title="注册"
-    right-text="注册"
+    :title="title"
+    :right-text="rightText"
     @click-right="onClickRight"
   ></van-nav-bar>
 </template>
 
-<style lang="scss" scoped> // 添加scoped避免样式污染(在调试中可看见生成复杂的data-v属性)
+<style lang="scss" scoped>
+// 添加scoped避免样式污染(在调试中可看见生成复杂的data-v属性)
 // ::v-deep() 深度作用选择符/样式穿透
 // 思考❓什么时候需要加, 什么时候不需要?
 // 1.元素身上有data-v属性, 直接拿类名控制
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,8 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const clickRight = () => {
+  console.log('点击了')
+}
+</script>
 
 <template>
   <!-- 使用了 unplugin-vue-components 默认 src/components 下组件会自动导入注册 -->
-  <cp-nav-bar></cp-nav-bar>>
+  <cp-nav-bar title="登录" rightText="注册" @click-right="clickRight"></cp-nav-bar>
 </template>
 
 <style lang="scss" scoped></style>
