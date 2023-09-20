b9d996442816d26576568580de324a5c6db9d546
zweirdo <1606403438@qq.com>
2023-09-20 14:26:10 +0800

    图标组件封装+声明组件类型

---
+++ src/components/cp-icon.vue
@@ -0,0 +1,20 @@
+<!-- 图标组件的封装 -->
+<script setup lang="ts">
+defineProps<{
+  name: string
+}>()
+</script>
+
+<template>
+  <svg aria-hidden="true" class="icon">
+    <!-- #icon-文件夹名称-图片名称 -->
+    <use :href="`#icon-${name}`" />
+  </svg>
+</template>
+
+<style lang="scss" scoped>
+.icon {
+  width: 1em;
+  height: 1em;
+}
+</style>
--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -2,6 +2,7 @@
 
 // 1. 导入组件实例
 import CpNavBar from '@/components/cp-nav-bar.vue'
+import CpIcon from '@/components/cp-icon.vue'
 // 2. 声明 vue 类型模块
 declare module 'vue' {
   // 3. 给 vue  添加全局组件类型，interface 和之前的合并
@@ -9,5 +10,6 @@ declare module 'vue' {
     // 4. 定义具体组件类型，typeof 获取到组件实例类型
     // typeof 作用是得到对应的TS类型
     CpNavBar: typeof CpNavBar
+    CpIcon: typeof CpIcon
   }
 }
