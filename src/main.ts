669576df248da156c47965320ee42af90eff248d
zweirdo <1606403438@qq.com>
2023-09-20 14:12:17 +0800

    打包精灵图

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import router from './router'
 // 导入全局样式
 import '@/styles/main.scss'
 
+// 注册svg图标
+import 'virtual:svg-icons-register'
+
 const app = createApp(App)
 
 app.use(pinia)
--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@ import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { VantResolver } from '@vant/auto-import-resolver'
 
+// 导入打包精灵图插件
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import path from 'path'
+
 export default defineConfig({
   // base: '/服务器中放置打包代码的目录名'
   plugins: [
@@ -16,6 +20,18 @@ export default defineConfig({
     Components({
       resolvers: [VantResolver()], // vant按需引入 => 项目下scr/components中的组件也会被自动全局注册, 因此不用导入
       dts: false // 关闭自动生成类型声明文件(不需要了, 已经有了)
+    }),
+    // 配置打包精灵图svg图片的位置
+    createSvgIconsPlugin({
+      // 指定图标文件夹，绝对路径（NODE代码）
+      iconDirs: [path.resolve(process.cwd(), 'src/icons')]
+      /**
+       * 之后svg的使用方式:
+       * <svg aria-hidden="true">
+            <!-- #icon-文件夹名称-图片名称 -->
+            <use href="#icon-login-eye-off" />
+         </svg>
+       */
     })
   ],
   resolve: {
