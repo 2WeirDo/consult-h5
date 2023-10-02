c0a018054686c4f95a58ca57177bc2b2fe1a9392
zweirdo <1606403438@qq.com>
2023-10-02 17:39:31 +0800

    问诊记录-订单状态功能按钮渲染

--- src/views/user/components/ConsultItem.vue
+++ src/views/user/components/ConsultItem.vue
@@ -1,7 +1,25 @@
 <script setup lang="ts">
 import type { ConsultOrderItem } from '@/types/consult'
 import { OrderType } from '@/enums'
-defineProps<{ item: ConsultOrderItem }>()
+import { computed, ref } from 'vue'
+
+// == 已完成订单使用 ==
+// 控制更多操作显示
+const showPopover = ref(false)
+// 操作项
+// 为什么用computed?  因为props也是异步传来的, 最开始可能没数据
+const actions = computed(() => [
+  // disabled是代表禁用(组件提供的字段)
+  { text: '查看处方', disabled: !props.item.prescriptionId }, // 没有开处方不能查看
+  { text: '删除订单' }
+])
+
+// 操作项的点击回调
+const onSelect = () => {
+  //
+}
+
+const props = defineProps<{ item: ConsultOrderItem }>()
 </script>
 
 <template>
@@ -36,9 +54,68 @@ defineProps<{ item: ConsultOrderItem }>()
         <div class="body-value tip">{{ item.createTime }}</div>
       </div>
     </div>
-    <div class="foot">
-      <van-button class="gray" plain size="small" round>取消订单</van-button>
-      <van-button type="primary" plain size="small" round to="/">去支付</van-button>
+
+    <!-- 根据不同的订单状态显示不同的操作按钮 -->
+    <!--   
+      待支付 ConsultPay
+      待接诊 ConsultWait
+      咨询中 ConsultChat
+      已完成 ConsultComplete
+      已取消 ConsultCancel
+    -->
+    <!-- 1. 待支付：取消问诊+去支付 -->
+    <div class="foot" v-if="item.status === OrderType.ConsultPay">
+      <van-button type="danger" plain size="small" round>取消问诊</van-button>
+      <van-button type="primary" plain size="small" round :to="`/user/consult/${item.id}`">
+        去支付
+      </van-button>
+    </div>
+    <!-- 2. 待接诊：取消问诊+继续沟通 -->
+    <div class="foot" v-if="item.status === OrderType.ConsultWait">
+      <van-button type="danger" plain size="small" round>取消问诊</van-button>
+      <van-button type="primary" plain size="small" round :to="`/room?orderId=${item.id}`">
+        继续沟通
+      </van-button>
+    </div>
+    <!-- 3. 咨询中：查看处方（如果开了）+继续沟通 -->
+    <div class="foot" v-if="item.status === OrderType.ConsultChat">
+      <van-button v-if="item.prescriptionId" type="primary" plain size="small" round>
+        查看处方
+      </van-button>
+      <van-button type="primary" plain size="small" round :to="`/room?orderId=${item.id}`">
+        继续沟通
+      </van-button>
+    </div>
+    <!-- 4. 已完成：更多（查看处方，如果开了，删除订单）+问诊记录+（未评价?写评价:查看评价） -->
+    <div class="foot" v-if="item.status === OrderType.ConsultComplete">
+      <div class="more">
+        <!-- van-popover组件下拉菜单, 因为一行放不下-->
+        <!-- 通过action指定下拉选项 : text代表文字 -->
+        <van-popover
+          theme="dark"
+          placement="top-start"
+          v-model:show="showPopover"
+          :actions="actions"
+          @select="onSelect"
+        >
+          <template #reference>
+            <van-button size="small" type="default">更多</van-button>
+          </template>
+        </van-popover>
+      </div>
+      <van-button type="warning" plain size="small" round :to="`/room?orderId=${item.id}`">
+        问诊记录
+      </van-button>
+      <van-button v-if="!item.evaluateId" type="primary" plain size="small" round>
+        <!-- 这里没有实现去评价的功能 -->
+        去评价
+      </van-button>
+      <van-button v-else plain type="success" size="small" round> 已评价 </van-button>
+    </div>
+    <!-- 5. 已取消：删除订单+咨询其他医生 -->
+    <div class="foot" v-if="item.status === OrderType.ConsultCancel">
+      <van-button type="danger" plain size="small" round>删除订单</van-button>
+      <van-button type="primary" plain size="small" round to="/">咨询其他医生</van-button>
     </div>
   </div>
 </template>
